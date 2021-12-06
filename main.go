c23e7226e7af3993fb56fa98cce2a1898584b4be
shigahiro <hiroki2131@icloud.com>
2021-12-06 14:41:27 +0900

    各関数のルーティング完了

--- main.go
+++ main.go
@@ -13,50 +13,75 @@ import (
 func main() {
 	router := gin.Default()
 	// HTMLファイルを読み込んだ結果をHTMLを描画するレンダリングエンジンというものに関連付ける
-	// router.LoadHTMLGlob("views/*.html")
+	router.LoadHTMLGlob("views/*.html")
 
 	db.Init()
-	// 登録
-	router.POST("/new", Register(c))
-	// 登録内容詳細
-	router.GET("/detail/:id")
-	// 一覧
-	router.GET("/")
-	// 更新
-	router.POST("/update/:id")
-	// 削除
-	router.POST("/delete/:id")
-	// 削除内容
-	router.GET("/delete_check/:id")
+	router.GET("/", func(c *gin.Context) {
+		tweets := db.GetAll()
+		c.HTML(200, "index.html", gin.H{"tweets": tweets})
+	})
+
+	//登録
+	router.POST("/new", func(c *gin.Context) {
+		var form model.Tweet
+		// 構造体のタグでバリデーションができる
+		if err := c.Bind(&form); err != nil {
+			tweets := db.GetAll()
+			c.HTML(http.StatusBadRequest, "index.html", gin.H{"tweets": tweets, "err": err})
+			// 他のハンドラーの呼び出しを防ぐ
+			c.Abort()
+		} else {
+			content := c.PostForm("content")
+			db.Insert(content)
+			c.Redirect(302, "/")
+		}
+	})
+
+	//投稿詳細
+	router.GET("/detail/:id", func(c *gin.Context) {
+		n := c.Param("id") // URLのパラメータの値を返す→今回はidに入ったもの
+		id, err := strconv.Atoi(n)
+		if err != nil {
+			panic(err)
+		}
+		tweet := db.GetOne(id)
+		c.HTML(200, "detail.html", gin.H{"tweet": tweet})
+	})
+
+	//削除確認
+	router.GET("/delete_check/:id", func(c *gin.Context) {
+		n := c.Param("id")
+		id, err := strconv.Atoi(n)
+		if err != nil {
+			panic("ERROR")
+		}
+		tweet := db.GetOne(id)
+		c.HTML(200, "delete.html", gin.H{"tweet": tweet})
+	})
+
+	//更新
+	router.POST("/update/:id", func(c *gin.Context) {
+		n := c.Param("id")
+		id, err := strconv.Atoi(n)
+		if err != nil {
+			panic("ERROR")
+		}
+		tweet := c.PostForm("tweet")
+		db.Update(id, tweet)
+		c.Redirect(302, "/")
+	})
+
+	//削除
+	router.POST("/delete/:id", func(c *gin.Context) {
+		n := c.Param("id")
+		id, err := strconv.Atoi(n)
+		if err != nil {
+			panic("ERROR")
+		}
+		db.Delete(id)
+		c.Redirect(302, "/")
+
+	})
 
 	router.Run()
-}
-
-func getParamId(c *gin.Context) model.Tweet {
-	n := c.Param("id")
-	id, err := strconv.Atoi(n)
-	if err != nil {
-		panic(err)
-	}
-	return db.GetOne(id)
-}
-
-func Register(c *gin.Context) {
-	var form model.Tweet
-	// 構造体のタグでバリデーションができる
-	if err := c.Bind(&form); err != nil {
-		tweets := db.GetAll()
-		c.HTML(http.StatusBadRequest, "index.html", gin.H{"tweets": tweets, "err": err})
-		// 他のハンドラーの呼び出しを防ぐ
-		c.Abort()
-	} else {
-		content := c.PostForm("content")
-		db.Insert(content)
-		c.Redirect(302, "/")
-	}
-}
-
-func GetDetail(c *gin.Context) {
-	tweet := getParamId(c)
-	c.HTML(200, "detail.html", gin.H{"tweet": tweet})
 }
